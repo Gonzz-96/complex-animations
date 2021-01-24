980dbb2c7017bf766fae0326f4fa6334f34d94b0
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-24 17:20:54 -0600

    Craete splash screen with list view builder.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primaryColor: Colors.white,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,8 +1,30 @@
+import 'package:complex_animations/screens/squared_circle/squared_circle_screen.dart';
+import 'package:complex_animations/widgets/screen_item.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  final screens = {
+    'Squared Circle': SquaredCircleScreen(),
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Complex Animations'),
+        elevation: 5.0,
+      ),
+      body: ListView.builder(
+        itemCount: screens.length,
+        itemBuilder: (context, index) {
+          final entry = screens.entries.toList()[index];
+          return ScreenItem(
+            entry: entry,
+            shouldHaveDivider: index != screens.length - 1,
+          );
+        },
+      ),
+    );
   }
 }
