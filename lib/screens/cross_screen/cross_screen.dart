eedccc5c42638b5bb31d8f4c0dde698d9a520770
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-26 22:04:06 -0600

    Refactor circle canvas.

--- lib/screens/cross_screen/cross_screen.dart
+++ lib/screens/cross_screen/cross_screen.dart
@@ -11,38 +11,58 @@ class CrossScreen extends StatelessWidget {
     final space = size.width * 0.02;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SquaredCircle(
-                  squareSize: squareSize,
-                  rotationAngle: Math.pi / 2,
-                ),
-                SizedBox(width: space),
-                SquaredCircle(
-                  squareSize: squareSize,
-                  rotationAngle: Math.pi,
-                ),
-              ],
-            ),
-            SizedBox(height: space),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SquaredCircle(squareSize: squareSize, rotationAngle: 0),
-                SizedBox(width: space),
-                SquaredCircle(
-                  squareSize: squareSize,
-                  rotationAngle: 3 * Math.pi / 2,
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          _CirclesCanvas(squareSize: squareSize, space: space),
+        ],
+      ),
+    );
+  }
+}
+
+class _CirclesCanvas extends StatelessWidget {
+  const _CirclesCanvas({
+    @required this.squareSize,
+    @required this.space,
+  });
+
+  final double squareSize;
+  final double space;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SquaredCircle(
+                squareSize: squareSize,
+                rotationAngle: Math.pi / 2,
+              ),
+              SizedBox(width: space),
+              SquaredCircle(
+                squareSize: squareSize,
+                rotationAngle: Math.pi,
+              ),
+            ],
+          ),
+          SizedBox(height: space),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SquaredCircle(squareSize: squareSize, rotationAngle: 0),
+              SizedBox(width: space),
+              SquaredCircle(
+                squareSize: squareSize,
+                rotationAngle: 3 * Math.pi / 2,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
