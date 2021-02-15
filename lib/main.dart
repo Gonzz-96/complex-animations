4752b9ae31614703c2300e71506a4ddbd67f5472
Gonzalo Campos <erick.campos@wizeline.com>
2021-02-14 22:57:36 -0600

    Create animation with tweens.

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,49 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class CustomImplicitAnimationsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: ColorTweenDitto()),
+    );
+  }
+}
+
+class ColorTweenDitto extends StatefulWidget {
+  @override
+  _ColorTweenDittoState createState() => _ColorTweenDittoState();
+}
+
+class _ColorTweenDittoState extends State<ColorTweenDitto> {
+  Color _backgroundColor = Colors.blueAccent;
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      tween: ColorTween(begin: Colors.blueAccent, end: _backgroundColor),
+      duration: Duration(seconds: 2),
+      builder: (context, color, child) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset('assets/ditto.png'),
+              color: color,
+            ),
+            FlatButton(onPressed: _changeColor, child: Text('Animate'))
+          ],
+        );
+      },
+    );
+  }
+
+  void _changeColor() {
+    setState(() {
+      _backgroundColor = _backgroundColor == Colors.blueAccent
+          ? Colors.redAccent
+          : Colors.blueAccent;
+    });
+  }
+}
